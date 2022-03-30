b462f6e672d356b97f203d3718d111231e91ced0
K4UNG <kgzinhein.my@gmail.com>
2022-03-30 12:39:44 +0630

    first finished version

--- script.js
+++ script.js
@@ -4,3 +4,35 @@ const nav = document.querySelector('nav')
 menu.onclick = () => {
     nav.classList.toggle('shown')
 }
+
+function adjustBg() {
+    const body = document.body
+
+    const hero = document.querySelector('.hero')
+
+    const images = document.querySelectorAll('.img img')
+    const works = document.querySelectorAll('.container-work')
+    const grid = document.querySelectorAll('.grid-container img')
+
+    let path = (body.clientWidth > 770 ? "desktop" : "mobile")
+
+    hero.style.backgroundImage = `url(${(hero.dataset.path).replace(/mobile|desktop/, path)})`
+
+    images.forEach(img => {
+        const newImg = img.getAttribute('src').replace(/mobile|desktop/, path)
+        img.setAttribute('src', newImg)
+    })
+
+    works.forEach(img => {
+        const imgPath = (img.dataset.path).replace(/mobile|desktop/, path);
+        img.style.backgroundImage = `url(${imgPath})`
+    })
+
+    grid.forEach(img => {
+        const newImg = img.getAttribute('src').replace(/mobile|desktop/, path)
+        img.setAttribute('src', newImg)
+    })
+}
+
+adjustBg()
+window.onresize = adjustBg
